9e61471a08102a3e20692af4366d96904fd148b6
Pedro Augusto <pedro.link25@gmail.com>
2022-10-27 21:18:24 -0300

    First Commit

---
+++ crud/src/main/java/com/crud/java/rest/CrudController.java
@@ -0,0 +1,9 @@
+package com.crud.java.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/crud")
+public class CrudController {
+}
